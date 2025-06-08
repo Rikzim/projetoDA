9ff86ffcb07a370093e41ebac8ec0ec1c5460b7d
MartimMendesIPL <2241559@my.ipleiria.pt>
2025-06-08 15:50:17 +0100

    Update TarefaController.cs

--- Projeto/Controllers/TarefaController.cs
+++ Projeto/Controllers/TarefaController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
